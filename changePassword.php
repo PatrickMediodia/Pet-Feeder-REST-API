6b3d7c5323bc71022aa7171fcc74d2c8272aa86d
Pat <50007415+PatrickMediodia@users.noreply.github.com>
2022-06-09 12:38:48 +0800

    Added change password functionality

--- changePassword.php
+++ changePassword.php
@@ -6,13 +6,28 @@ include_once('Models.php');
 $old_password = $_GET['oldPassword'];
 $new_password = $_GET['newPassword'];
 
-$query_string = "UPDATE account SET `password` = '$new_password'";
+$query_string = "SELECT `password` FROM account";
 $result = mysqli_query($con, $query_string);
+$row = $result->fetch_assoc();
 
-if($result) {
-    echo "Account Password Updated";
-} else {
-    echo "Error";
+if($new_password == "") 
+    echo "New password must not be empty";
+    
+else if ($old_password != $row['password']) 
+    echo "Wrong old password";
+
+else if ($old_password == $new_password) 
+    echo "New password must be different from old password";
+    
+else {
+    $query_string = "UPDATE account SET `password` = '$new_password'";
+    $result = mysqli_query($con, $query_string);
+    
+    if($result) {
+        echo "Account Password Updated";
+    } else {
+        echo "Error";
+    }
 }
 
 mysqli_close($con);
